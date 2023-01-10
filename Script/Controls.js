f35f64099280a429e04de7b5fb01c320067d1a76
dylululu <duynhatluu2003@gmail.com>
2023-01-10 13:34:37 +0700

    m

---
+++ Script/Controls.js
@@ -0,0 +1,12 @@
+pauseAfterSeconds("Xuan",15);
+
+pauseAfterSeconds("GioLa",5);
+
+pauseAfterSeconds("Buon1",8);
+
+pauseAfterSeconds("ThoiCom",10);
+
+pauseAfterSeconds("NuCuoi",40);
+
+// tangDan("NuCuoi",0,0.9,0.1,0.5);
+
