0c5621a3bc226870770e8151ca2e466706445df6
dylululu <duynhatluu2003@gmail.com>
2023-10-05 15:50:02 +0700

    m

--- Script/Controls.js
+++ Script/Controls.js
@@ -1,25 +1,10 @@
-pauseAfterSeconds("Xuan",15);
+audioStartsAt("sp",0.5);
+audioStartsAt("toilet",3);
+audioStartsAt("door",2);
+audioStartsAt("chuoi",1.5);
 
-pauseAfterSeconds("GioLa",10);
 
-// pauseAfterSeconds("Buon1",60);
 
-setVolume("Buon1",0.3);
 
-pauseAfterSeconds("Buon2",8);
 
-pauseAfterSeconds("ThoiCom",10);
 
-// pauseAfterSeconds("NuCuoi",40);
-
-// pauseAfterSeconds("Mixi",60);
-
-// tangDan("NuCuoi",0,0.9,0.1,0.5);
-
-//pauseAfterSeconds("WC",2);
-
-setVolume("Mixi",0.3);
-
-pauseAfterSeconds("TQ",10);
-
-pauseAfterSeconds("GTA",2);
